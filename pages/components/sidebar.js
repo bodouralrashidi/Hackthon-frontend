95fe736f196eb967bcfdfb1a312d12a7b0baf9e6
Athbi989 <70595484+Athbi989@users.noreply.github.com>
2022-09-24 12:20:14 +0300

    upload temp page

--- pages/components/sidebar.js
+++ pages/components/sidebar.js
@@ -7,6 +7,7 @@ import SyncAltIcon from "@material-ui/icons/SyncAlt";
 import LayersIcon from "@material-ui/icons/Layers";
 import LockIcon from "@material-ui/icons/Lock";
 import EcoIcon from "@material-ui/icons/Eco";
+import Link from "next/link";
 
 const Sidebar = () => {
   return (
@@ -19,36 +20,18 @@ const Sidebar = () => {
       <div className="p-4 space-y-14">
         <div className="space-y-4">
           <h1 className="text-gray-400">Menu</h1>
-          <div className="">
+          <Link href="/">
             <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
               <DonutLargeIcon className=" text-gray-300" />
               <p className=" ">Main Menu</p>
             </div>
-          </div>
-          <div className="">
+          </Link>
+          <Link href="/page2">
             <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-              <ClearAllIcon className="text-gray-300" />
-              <p className="text-gray-600  ">Transaction</p>
+              <DonutLargeIcon className=" text-gray-300" />
+              <p className=" ">Page template</p>
             </div>
-          </div>
-          <div className="">
-            <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-              <ArrowUpwardIcon className="text-gray-300" />
-              <p className="text-gray-600  ">Send</p>
-            </div>
-          </div>
-          <div className="">
-            <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-              <ArrowDownwardIcon className="text-gray-300" />
-              <p className="text-gray-600  ">Receive</p>
-            </div>
-          </div>
-          <div className="">
-            <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer  ">
-              <SyncAltIcon className="text-gray-300" />
-              <p className="text-gray-600  ">Buy</p>
-            </div>
-          </div>
+          </Link>
         </div>
       </div>
     </div>
---
+++ pages/page2.js
@@ -0,0 +1,5 @@
+function name(props) {
+  return <section className="p-5">This is new page template</section>;
+}
+
+export default name;
